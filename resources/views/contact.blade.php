31fd2dfdec007f7c5fa35c3258a9f798294322db
tomohitokamiyama <itinogi@gmail.com>
2024-01-16 12:32:15 +0000

    first commit

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,8 +5,28 @@
    <title>カフェ</title>
 </head>
 <body>
-    <title>Cafeterian お問い合わせページ</title>
-    <h3>あ</h3>
+    <div class="wrapper">
+        <title>Cafeterian お問い合わせページ</title>
+        <h2 class="page-title">お問い合わせ</h2>
+        <form action="#">
+            <div>
+                <label for="name">お名前</label>
+                <input type="text" id="name" name="your-name">
+            </div>
+            
+            </div>
+                <label for="email">メールアドレス</label>
+                <input type="email" id="email" name="your-email">
+            </div>
+            
+            <div>
+                <larabel for="message">メッセージ</label>
+                <textarea id="message" name="your-message"></textarea>
+            </div>
+            
+            <input type="submit" class="button" value="送信">
+        <form>
+    </div>
     
     
     
